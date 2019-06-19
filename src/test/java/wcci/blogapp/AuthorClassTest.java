4dbefd62d5dd1187de06e7b7fdee0fa7bcfeef0f
Sshipley12 <steph.shipley@yahoo.com>
2019-06-19 09:36:22 -0400

    process of writing first test

---
+++ src/test/java/wcci/blogapp/AuthorClassTest.java
@@ -0,0 +1,24 @@
+package wcci.blogapp;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class AuthorClassTest {
+
+	private LocalDateTime dateTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
+	Post postOne = new Post("title", "body", "authors", dateTime, "genre", "tags");
+	private LocalDateTime dateTime2 = LocalDateTime.of(2016, Month.JULY, 29, 19, 30, 40);
+	Post postTwo = new Post("title2", "body2", "authors2", dateTime2, "genre2", "tags2");
+	
+	Collection<Post> posts;
+	posts.add
+	
+	@Test
+	public void authorShouldHaveNamePosts() {
+		Author underTest = new Author("name",  )
+	}
+	
+}
