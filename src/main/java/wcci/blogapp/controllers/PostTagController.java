8cade29a72463223272435743adb354009fd5946
matthew-laine <matthew.i.laine@gmail.com>
2019-06-30 18:29:35 -0400

    post tag controller can add post tag

--- src/main/java/wcci/blogapp/controllers/PostTagController.java
+++ src/main/java/wcci/blogapp/controllers/PostTagController.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import wcci.blogapp.models.PostTag;
 import wcci.blogapp.repositories.PostTagRepository;
 
 @Controller
@@ -21,6 +22,26 @@ public class PostTagController {
 	public String findById(long id, Model model) {
 		model.addAttribute("singlePostTagAttribute", postTagRepo.findById(id).get());
 		return "singlePostTagTemplate";
+	}
+	
+	public String saveNewPostTag(String name) {
+		boolean shouldAddPostTag = shouldAddPostTag(name);
+		if (shouldAddPostTag) {
+			PostTag postTagToAdd = new PostTag(name);
+			postTagRepo.save(postTagToAdd);
+		}
+		return "redirect:/post-tags";
+	}
+
+	private boolean shouldAddPostTag(String name) {
+		Iterable<PostTag> allPostTags = postTagRepo.findAll();
+		boolean addPostTag = true;
+		for(PostTag postTag : allPostTags) {
+			if(postTag.getName().equals(name)) {
+				addPostTag = false;
+			}
+		}
+		return addPostTag;
 	}
 
 }
